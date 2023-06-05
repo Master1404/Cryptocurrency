f3f892b8528f55d44f3bab405e94d5f3872e0aa1
Master1404 <110604758+Master1404@users.noreply.github.com>
2023-06-05 13:00:30 +0300

    Started and created CryptocurrencyModel

---
+++ Cryptocurrency/Cryptocurrency/Cryptocurrency/Models/CryptocurrencyModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cryptocurrency.Models
+{
+    public class CryptocurrencyDataModel
+    {
+        public string Id { get; set; }
+        public int Rank { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public decimal Supply { get; set; }
+        public Nullable<decimal> MaxSupply { get; set; }
+        public decimal MarketCapUsd { get; set; }
+        public decimal VolumeUsd24Hr { get; set; }
+        public decimal PriceUsd { get; set; }
+        public decimal ChangePercent24Hr { get; set; }
+        public Nullable<decimal> Vwap24Hr { get; set; }
+    }
+}
